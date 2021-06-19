67ac64753a5ac73faa42c1c20831037f44e5cd26
Azzam <varokahdotnet@gmail.com>
2021-06-19 14:34:44 +0800

    Tes requests ketiga (pagination - author) ke quotes

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests as requests
 
-r = requests.get('http://quotes.toscrape.com/')
-html = r.text
-with open('quotes.txt', 'w') as f:
-    for line in html.split('\n'):
-        if '<span class="text" itemprop="text">' in line:
-            line = line.replace('<span class="text" itemprop="text">“', '').replace('”</span>', '')
-            line = line.strip()
-            f.write(line)
-            f.write("\n")
+for i in range(1, 11):
+    print('Page: ',i)
+    url = f'http://quotes.toscrape.com/page/{i}'
+    r = requests.get(url)
+    html = r.text
+    with open('quotes.txt', 'a', encoding='utf-8') as f:
+        for line in html.split('\n'):
+            if '<span class="text" itemprop="text">' in line:
+                line = line.replace('<span class="text" itemprop="text">“', '').replace('”</span>', '')
+                line = line.strip()
+                f.write(line)
+                f.write("\n")
